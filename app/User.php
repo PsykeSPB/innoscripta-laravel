3d25d33812ce39115a3725f0b5cb5e962aabd9e0
psykespb <psykespb@gmail.com>
2019-12-19 18:05:37 +0300

    Changed links to class literals

--- app/User.php
+++ app/User.php
@@ -42,6 +42,6 @@ class User extends Authenticatable
      */
     public function orders()
     {
-        return $this->hasMany('App\Order');
+        return $this->hasMany(Order::class);
     }
 }
