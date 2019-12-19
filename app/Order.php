17516cbbc0f89080f6d0d122726d7441a5d749ca
psykespb <psykespb@gmail.com>
2019-12-19 18:03:18 +0300

    Added 'Order' model with 'User' relation

---
+++ app/Order.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'phone', 'address', 'comment'];
+
+    /**
+     * Get the user that made this order.
+     * May return null, if user hasn't been loged in.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get orders of this user.
+     */
+    public function orders()
+    {
+        return $this->hasMany('App\Order');
+    }
 }
